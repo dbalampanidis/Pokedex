42ee94c01638097b3a52995977c158df5556d5ca
dbalampanidis <dbalampanidis@gmail.com>
2022-09-10 22:16:44 +0300

    display types and type colors

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const poke_container = document.querySelector('#poke_container');
 const pokemon_number = 150;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+
+const main_types = Object.keys(colors);
 
 const fetchPokemon = async () => {
 	for (let i = 1; i <= pokemon_number; i++) {
@@ -18,9 +36,13 @@ function createPokemonCard(pokemon) {
 	const pokemonEl = document.createElement('div');
 	pokemonEl.classList.add('pokemon');
 
+    const poke_types = pokemon.types.map(type => type.type.name);
+	const type = main_types.find(type => poke_types.indexOf(type) > -1);
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
-
+    const color = colors[type];
+	
+	pokemonEl.style.backgroundColor = color;
 	const pokeInnerHTML = `
         <div class="img-container">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
@@ -32,7 +54,7 @@ function createPokemonCard(pokemon) {
 							.toString()
 							.padStart(3, '0')}</span>
             <h3 class="name">${name}</h3>
-            
+            <small class="type">Type: <span>${type}</span></small>
         </div>
     `;
 
