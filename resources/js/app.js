39b238abfe71b1a154948f9e00c21aed637ae9af
Daniel Balampanidis <dbalampanidis@gmail.com>
2023-02-14 15:29:26 +0200

    organise code, add favicon

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,71 +2,73 @@ const poke_container = document.querySelector('#poke_container');
 const pokemon_number = 151;
 
 const fetchPokemon = async () => {
-	for (let i = 1; i <= pokemon_number; i++) {
-		await getPokemon(i);
-	}
+  for (let i = 1; i <= pokemon_number; i++) {
+    await getPokemon(i);
+  }
 };
 
 const getPokemon = async id => {
-	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-	const res = await fetch(url);
-	const pokemon = await res.json();
-	createPokemonCard(pokemon);
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  createPokemonCard(pokemon);
 };
 
+//create each pokemon card
 function createPokemonCard(pokemon) {
-	const pokemonEl = document.createElement('div');
-	pokemonEl.classList.add('pokemon');
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
 
-	const pokemonTypes = pokemon.types.map(type => type.type.name);
-	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const pokemonTypes = pokemon.types.map(type => type.type.name);
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
 
-	const pokeInnerHTML = `
+  const pokeInnerHTML = `
         <div class="img-container">
             <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id
-			.toString()
-			.padStart(3, '0')}.png" alt="${name}" />
+      .toString()
+      .padStart(3, '0')}.png" alt="${name}" />
         </div>
         <div class="info">
             <span class="number">#${pokemon.id
-			.toString()
-			.padStart(3, '0')}</span>
+      .toString()
+      .padStart(3, '0')}</span>
             <h3 class="name">${name}</h3>
             
 			${pokemonTypes.map(type => `<div class="type-box ${type}">${type.charAt(0).toUpperCase() + type.slice(1)}</div>`).join("")}
         </div>
     `;
 
-	pokemonEl.innerHTML = pokeInnerHTML;
+  pokemonEl.innerHTML = pokeInnerHTML;
 
-	poke_container.appendChild(pokemonEl);
-
-
-
-	pokemonEl.addEventListener('click', () => {
-		showPokemonDetails(pokemon);
-	});
+  poke_container.appendChild(pokemonEl);
 
 
-	function showPokemonDetails(pokemon) {
-		const pokemonDetails = document.querySelector('#pokemon-details');
-		const modal = document.querySelector('#modal');
-		pokemonDetails.classList.add("container");
 
-		const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-		const imageSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id.toString().padStart(3, '0')}.png`;
-		const height = pokemon.height;
-		const weight = pokemon.weight;
-		const abilities = pokemon.abilities.map(a => a.ability.name.charAt(0).toUpperCase() + a.ability.name.slice(1)).join(", ");
-		const descriptionUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`;
+  //show pokemon details when clicking on card logic
+  pokemonEl.addEventListener('click', () => {
+    showPokemonDetails(pokemon);
+  });
 
-		fetch(descriptionUrl)
-			.then(res => res.json())
-			.then(data => {
-				const description = data.flavor_text_entries.find(entry => entry.language.name === "en").flavor_text.replace(/\u00A0/g, ' ');
 
-				pokemonDetails.innerHTML = `
+  function showPokemonDetails(pokemon) {
+    const pokemonDetails = document.querySelector('#pokemon-details');
+    const modal = document.querySelector('#modal');
+    pokemonDetails.classList.add("container");
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const imageSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id.toString().padStart(3, '0')}.png`;
+    const height = pokemon.height;
+    const weight = pokemon.weight;
+    const abilities = pokemon.abilities.map(a => a.ability.name.charAt(0).toUpperCase() + a.ability.name.slice(1)).join(", ");
+    const descriptionUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`;
+
+    fetch(descriptionUrl)
+      .then(res => res.json())
+      .then(data => {
+        const description = data.flavor_text_entries.find(entry => entry.language.name === "en").flavor_text.replace(/\u00A0/g, ' ');
+
+        pokemonDetails.innerHTML = `
 			<div class="row row-margin mb-3">
 				<div class="col text-center">
 					<h3 class="name">${name}</h3>
@@ -152,16 +154,16 @@ function createPokemonCard(pokemon) {
     </div>
   </div>
 			`;
-			});
+      });
 
-		modal.style.display = 'block';
+    modal.style.display = 'block';
 
-		window.addEventListener('click', event => {
-			if (event.target === modal) {
-				modal.style.display = 'none';
-			}
-		});
-	}
+    window.addEventListener('click', event => {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  }
 
 }
 
