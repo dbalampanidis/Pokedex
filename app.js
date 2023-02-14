3f81892f331ba7345a621b38721b664890f4be8f
Daniel Balampanidis <dbalampanidis@gmail.com>
2023-02-14 14:38:28 +0200

    add pokemon description when clicking on a pokemon(not completed)

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ const getPokemon = async id => {
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 	const res = await fetch(url);
 	const pokemon = await res.json();
-    createPokemonCard(pokemon);
+	createPokemonCard(pokemon);
 };
 
 function createPokemonCard(pokemon) {
 	const pokemonEl = document.createElement('div');
 	pokemonEl.classList.add('pokemon');
 
-    const pokemonTypes = pokemon.types.map(type => type.type.name);
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-	
-	
+	const pokemonTypes = pokemon.types.map(type => type.type.name);
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+
 	const pokeInnerHTML = `
         <div class="img-container">
             <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id
@@ -30,8 +30,8 @@ function createPokemonCard(pokemon) {
         </div>
         <div class="info">
             <span class="number">#${pokemon.id
-							.toString()
-							.padStart(3, '0')}</span>
+			.toString()
+			.padStart(3, '0')}</span>
             <h3 class="name">${name}</h3>
             
 			${pokemonTypes.map(type => `<div class="type-box ${type}">${type.charAt(0).toUpperCase() + type.slice(1)}</div>`).join("")}
@@ -46,36 +46,122 @@ function createPokemonCard(pokemon) {
 
 	pokemonEl.addEventListener('click', () => {
 		showPokemonDetails(pokemon);
-	  });
+	});
 
 
-	  function showPokemonDetails(pokemon) {
+	function showPokemonDetails(pokemon) {
 		const pokemonDetails = document.querySelector('#pokemon-details');
 		const modal = document.querySelector('#modal');
-		const closeModal = document.querySelector('#close-modal');
-		pokemonDetails.classList.add("details");
-		
+		pokemonDetails.classList.add("container");
 
-		pokemonDetails.innerHTML = `
-		<div class="title">
-    <h3>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
-	</div>
-	
-	
-  `;
+		const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+		const imageSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id.toString().padStart(3, '0')}.png`;
+		const height = pokemon.height;
+		const weight = pokemon.weight;
+		const abilities = pokemon.abilities.map(a => a.ability.name.charAt(0).toUpperCase() + a.ability.name.slice(1)).join(", ");
+		const descriptionUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}/`;
+
+		fetch(descriptionUrl)
+			.then(res => res.json())
+			.then(data => {
+				const description = data.flavor_text_entries.find(entry => entry.language.name === "en").flavor_text.replace(/\u00A0/g, ' ');
+
+				pokemonDetails.innerHTML = `
+			<div class="row row-margin mb-3">
+				<div class="col text-center">
+					<h3 class="name">${name}</h3>
+				</div>
+				
+			</div>
+			<div class="row row-margin mb-3">
+   			 <div class="col-5">
+      			<img src="${imageSrc}" alt="${name}" class="pokemon-sprite">
+    		</div>
+    	<div class="col-7">
+			<div class="row row-margin mb-3">
+			<div class="col">
+				<div class="description">
+				<p>${description}</p>
+				</div>
+			</div>
+		</div>
+      
+      <div class="row row-margin mb-3">
+        <div class="col">
+          <div class="stats">
+            <div class="row row-margin mb-1">
+              <div class="col">
+                <span class="stats-title"> Height </span>
+              </div>
+              <div class="col">
+                <span class="stats-title"> Category </span>
+              </div>
+            </div>
+            <div class="row row-margin mb-3">
+              <div class="col">
+                <span> ${height / 10} kg </span>
+              </div>
+              <div class="col">
+                <span> Category </span>
+              </div>
+            </div>
+            <div class="row row-margin mb-1">
+              <div class="col">
+                <span class="stats-title"> Weight </span>
+              </div>
+              <div class="col">
+                <span class="stats-title"> Abilites </span>
+              </div>
+            </div>
+            <div class="row row-margin mb-3">
+              <div class="col">
+                <span> ${weight / 10} kg </span>
+              </div>
+              <div class="col">
+                <span> ${abilities} </span>
+              </div>
+            </div>
+            <div class="row row-margin mb-1">
+              <div class="col">
+                <span class="stats-title"> Gender </span>
+              </div>
+            </div>
+            <div class="row row-margin mb-3">
+              <div class="col">
+                <span> Gender </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row-margin mb-1">
+        <div class="col">
+          Type
+        </div>
+      </div>
+      <div class="row row-margin mb-3">
+        <div class="col">
+          ${pokemonTypes.map(type => `<div class="type-box ${type}">${type.charAt(0).toUpperCase() + type.slice(1)}</div>`).join("")}
+        </div>
+      </div>
+	  <div class="row">
+				<div class="col">
+				Evolutions
+				</div>
+	  </div>
+    </div>
+  </div>
+			`;
+			});
 
 		modal.style.display = 'block';
 
-		closeModal.addEventListener('click', () => {
-			modal.style.display = 'none';
-		  });
-
-		  window.addEventListener('click', event => {
+		window.addEventListener('click', event => {
 			if (event.target === modal) {
-			  modal.style.display = 'none';
+				modal.style.display = 'none';
 			}
-		  });
-		}
+		});
+	}
 
 }
 
